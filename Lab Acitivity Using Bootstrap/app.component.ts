78ab0b7c8dc8bdbd8fd4d47fdb193fb3243bef38
roren06 <123538573+roren06@users.noreply.github.com>
2024-01-13 22:49:50 +0800

    Add files via upload

---
+++ Lab Acitivity Using Bootstrap/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'data-binding';
+  email="";
+
+  changeName(e:any){
+
+    this.email = e.target.value  // this is where i locate my data binding.
+
+  }
+}
